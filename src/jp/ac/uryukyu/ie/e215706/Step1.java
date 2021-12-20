5faca0fe1e078dd4351928fa8acce26c6f1467b2
kim hyunwoo <e215706@ie.u-ryukyu.ac.jp>
2021-12-21 03:26:44 +0900

    step2

--- src/jp/ac/uryukyu/ie/e215706/Step1.java
+++ src/jp/ac/uryukyu/ie/e215706/Step1.java
@@ -2,7 +2,13 @@ package jp.ac.uryukyu.ie.e215706;
 
 public class Step1 {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }catch(NullPointerException e) {
+            System.out.println("NullPointerException発生！");
+            System.out.println(e.getMessage());
+        }
+        
     }
 }
