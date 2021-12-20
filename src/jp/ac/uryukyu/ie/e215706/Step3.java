d9a59d164032b8443775a23fa06383e02b123298
kim hyunwoo <e215706@ie.u-ryukyu.ac.jp>
2021-12-21 04:37:56 +0900

    step3

---
+++ src/jp/ac/uryukyu/ie/e215706/Step3.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e215706;
+
+public class Step3 {
+    public static void main(String[] args) {
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+    }
+}
